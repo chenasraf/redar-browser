1bb870f3b6d5786697b1d28513daa04a0a7548dd
Chen Asraf <chen@asraf.me>
2018-01-15 00:54:31 +0200

    background cleanup

--- src/background.tsx
+++ src/background.tsx
@@ -1,15 +1,28 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios'
 
 chrome.browserAction.onClicked.addListener((activeTab) => {
     const newURL = chrome.extension.getURL('index.html')
     chrome.tabs.create({ url: newURL })
 })
 
+interface Action {
+  action: string
+  payload?: any
+}
+
+type Responder = (response: any) => void
+
 if (typeof chrome.runtime.onMessage.addListener === 'function') {
   chrome.runtime.onMessage.addListener(
-    (message: {action: string, payload?: any}, sender: any, sendResponse: (response: any) => void) => {
+    (message: Action, sender: any, sendResponse: Responder) => {
       if (message.action === 'request') {
-        axios.request(message.payload).then((response: AxiosResponse) => sendResponse(response))
+        axios.request(message.payload)
+          .then((response: AxiosResponse) => sendResponse(response))
+          .catch((error: AxiosError) => {
+            console.error(error)
+            console.log({ respone: error.response, request: error.request })
+            sendResponse(error)
+          })
       }
       return true
     })
